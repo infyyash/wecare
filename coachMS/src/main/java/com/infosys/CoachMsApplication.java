55b72850a411616bcac4db5f5d5f86b2739d0a36
infyyash <88081839+infyyash@users.noreply.github.com>
2021-08-16 18:17:40 +0530

    added coachMS

---
+++ coachMS/src/main/java/com/infosys/CoachMsApplication.java
@@ -0,0 +1,13 @@
+package com.infosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachMsApplication.class, args);
+	}
+
+}
